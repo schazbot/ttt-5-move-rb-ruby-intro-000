d31ccfa382dcdea4a6fb79922de6e64c2154a058
Charlotte Pearce <charlotte.pearce1984@gmail.com>
2019-01-09 16:49:55 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,19 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+board= [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+display_board(board)
+
+
+puts "Where would you like to go?"
+board= [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+display_board(board)
+end
+
+def input_to_index
+move = gets.strip
+token = "X"
+board[position.to_i-1] = token
+end
+
 # code your input_to_index and move method here!
